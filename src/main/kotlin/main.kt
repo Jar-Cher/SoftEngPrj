ddb9fca0e0387ee6084260d8cb8ab16702cac37a
Jaroslav Chernyshev <31724693+Jar-Cher@users.noreply.github.com>
2021-12-02 20:11:59 +0300

    Initial commit

---
+++ src/main/kotlin/main.kt
@@ -0,0 +1,10 @@
+fun main(args: Array<String>) {
+    println("Hello World!")
+}
+
+class WeatherWidget(args: Array<String>) {
+
+    fun main(args: Array<String>) {
+        println("Hello World!")
+    }
+}
