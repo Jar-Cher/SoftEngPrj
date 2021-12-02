dd8f892b181719139ad3013bba71a25d7ce65570
Jaroslav Chernyshev <31724693+Jar-Cher@users.noreply.github.com>
2021-12-02 23:05:13 +0300

    + getTemperature(city: String)

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,10 +1,24 @@
+import com.fasterxml.jackson.databind.ObjectMapper
+import org.apache.http.client.methods.HttpGet
+import org.apache.http.impl.client.HttpClients
+import org.apache.http.util.EntityUtils
+
+
 fun main(args: Array<String>) {
-    println("Hello World!")
+    WeatherWidget.getTemperature("Moscow")
 }
 
-class WeatherWidget(args: Array<String>) {
+class WeatherWidget() {
 
-    fun main(args: Array<String>) {
-        println("Hello World!")
+    companion object {
+        fun getTemperature(city: String) {
+            val httpClient = HttpClients.createDefault()
+            val get = HttpGet("http://api.openweathermap.org/data/2.5/weather?q=$city&appid=514618c1ab6dd23ac38d98bfc9ae6378")
+            val response = httpClient.execute(get)
+            val weatherString = EntityUtils.toString(response.entity)
+            val actualObj = ObjectMapper().readTree(weatherString)
+            println((actualObj.findValue("temp").toString().toDouble()-273.15).toInt())
+        }
     }
+
 }
