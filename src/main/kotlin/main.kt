3330c2def897bcef3e9f0440c8714815ea6a9d3e
Jaroslav Chernyshev <31724693+Jar-Cher@users.noreply.github.com>
2021-12-03 23:22:47 +0300

    + Exception catchers

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -4,14 +4,14 @@ import com.fasterxml.jackson.databind.ObjectMapper
 import org.apache.http.client.methods.HttpGet
 import org.apache.http.impl.client.HttpClients
 import org.apache.http.util.EntityUtils
-import org.intellij.lang.annotations.Language
 import java.io.File
+import java.io.IOException
 import java.util.*
 
 
 fun main(args: Array<String>) {
-    //WeatherWidget.getTemperature(134)
-    WeatherWidget.getTemperature("Saint Petersburg")
+    WeatherWidget.getTemperature(134)
+    //WeatherWidget.getTemperature("Saint Petersbug")
 }
 
 class WeatherWidget() {
@@ -21,17 +21,29 @@ class WeatherWidget() {
         private val citiesDB = ObjectMapper().readTree(File("city.list.json").readText())
 
         fun getTemperature(id: Int) {
-            val httpClient = HttpClients.createDefault()
-            val get = HttpGet("http://api.openweathermap.org/data/2.5/weather?id=$id&appid=514618c1ab6dd23ac38d98bfc9ae6378")
-            val response = httpClient.execute(get)
-            val weatherString = EntityUtils.toString(response.entity)
-            val actualObj = ObjectMapper().readTree(weatherString)
-            println("Current weather in " + actualObj.findValue("name").toString().trim { it=='\"' } + ":")
-            println("Temperature: " + (actualObj.findValue("temp").toString().toDouble()-273.15).toInt() + "°")
-            println(actualObj.findValue("description").toString()
-                .trim { it=='\"' }
-                .replaceFirstChar { if (it.isLowerCase()) it.titlecase(Locale.getDefault()) else it.toString() })
-            println("Humidity " + (actualObj.findValue("humidity").toString()).uppercase() + "%")
+            try {
+                val httpClient = HttpClients.createDefault()
+                val get =
+                    HttpGet("http://api.openweathermap.org/data/2.5/weather?id=$id&appid=514618c1ab6dd23ac38d98bfc9ae6378")
+                val response = httpClient.execute(get)
+                val weatherString = EntityUtils.toString(response.entity)
+                val actualObj = ObjectMapper().readTree(weatherString)
+                try {
+                    println("Current weather in " + actualObj.findValue("name").toString().trim { it == '\"' } + ":")
+                    println(
+                        "Temperature: " + (actualObj.findValue("temp").toString().toDouble() - 273.15).toInt() + "°"
+                    )
+                    println(actualObj.findValue("description").toString()
+                        .trim { it == '\"' }
+                        .replaceFirstChar { if (it.isLowerCase()) it.titlecase(Locale.getDefault()) else it.toString() })
+                    println("Humidity " + (actualObj.findValue("humidity").toString()).uppercase() + "%")
+                } catch (e: NullPointerException) {
+                    println("Error: found no city with id# $id")
+                }
+                httpClient.close()
+            } catch (e: IOException) {
+                println("Network error")
+            }
         }
 
         fun getTemperature(city: String) {
@@ -40,7 +52,11 @@ class WeatherWidget() {
                 .readValue(citiesDB.findValues("name").toString(), object: TypeReference<List<String>>() {})
                 .indexOf(city)
             //println(cityIds[cityOrder].asInt())
-            getTemperature(cityIds[cityOrder].asInt())
+            try {
+                getTemperature(cityIds[cityOrder].asInt())
+            } catch (e: IndexOutOfBoundsException) {
+                println("Error: found no city named $city")
+            }
         }
     }
 
