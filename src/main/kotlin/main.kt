2fb9f4c047759f50c1f6327dbfa48a5b4ac72209
Jaroslav Chernyshev <31724693+Jar-Cher@users.noreply.github.com>
2021-12-03 22:31:23 +0300

    + getTemperature(city: id)

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,23 +1,41 @@
+
+import com.fasterxml.jackson.core.type.TypeReference
 import com.fasterxml.jackson.databind.ObjectMapper
 import org.apache.http.client.methods.HttpGet
 import org.apache.http.impl.client.HttpClients
 import org.apache.http.util.EntityUtils
+import org.intellij.lang.annotations.Language
+import java.io.File
+import java.util.*
 
 
 fun main(args: Array<String>) {
-    WeatherWidget.getTemperature("Moscow")
+    //WeatherWidget.getTemperature(134)
+    WeatherWidget.getTemperature("Saint Petersburg")
 }
 
 class WeatherWidget() {
 
     companion object {
-        fun getTemperature(city: String) {
+
+        private val citiesDB = ObjectMapper().readTree(File("city.list.json").readText())
+
+        fun getTemperature(id: Int) {
             val httpClient = HttpClients.createDefault()
-            val get = HttpGet("http://api.openweathermap.org/data/2.5/weather?q=$city&appid=514618c1ab6dd23ac38d98bfc9ae6378")
+            val get = HttpGet("http://api.openweathermap.org/data/2.5/weather?id=$id&appid=514618c1ab6dd23ac38d98bfc9ae6378")
             val response = httpClient.execute(get)
             val weatherString = EntityUtils.toString(response.entity)
             val actualObj = ObjectMapper().readTree(weatherString)
             println((actualObj.findValue("temp").toString().toDouble()-273.15).toInt())
+        }
+
+        fun getTemperature(city: String) {
+            val cityIds = citiesDB.findValues("id")
+            val cityOrder = ObjectMapper()
+                .readValue(citiesDB.findValues("name").toString(), object: TypeReference<List<String>>() {})
+                .indexOf(city)
+            //println(cityIds[cityOrder].asInt())
+            getTemperature(cityIds[cityOrder].asInt())
         }
     }
 
